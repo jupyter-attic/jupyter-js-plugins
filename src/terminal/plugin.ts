dadc8f46b36775fb29dfa83d72c05de9e0d6d4b5
A. Darian <git@darian.af>
2016-02-18 15:45:33 +0000

    merge conflict resolution

--- src/terminal/plugin.ts
+++ src/terminal/plugin.ts
@@ -7,17 +7,8 @@ import {
 } from 'jupyter-js-terminal';
 
 import {
-<<<<<<< HEAD
-  IAppShell, ICommandPalette, ICommandRegistry
-} from 'phosphide';
-
-import {
-  Container, Token
-} from 'phosphor-di';
-=======
   Application
 } from 'phosphide/lib/core/application';
->>>>>>> master
 
 import {
   TabPanel
@@ -25,47 +16,9 @@ import {
 
 
 /**
- * The default terminal extension. 
+ * The default terminal extension.
  */
 export
-<<<<<<< HEAD
-function resolve(container: Container): Promise<void> {
-  return container.resolve({
-    requires: [IAppShell, ICommandPalette, ICommandRegistry],
-    create: (shell: IAppShell, palette: ICommandPalette, registry: ICommandRegistry) => {
-      let newTerminalId = 'terminal:new';
-
-      registry.add([
-        {
-          id: newTerminalId,
-          handler: () => {
-            let term = new TerminalWidget();
-            term.color = 'black';
-            term.background = 'white';
-            term.title.closable = true;
-            shell.addToMainArea(term);
-            let stack = term.parent;
-            if (!stack) {
-              return;
-            }
-            let tabs = stack.parent;
-            if (tabs instanceof TabPanel) {
-              tabs.currentWidget = term;
-            }
-          }
-        }
-      ]);
-      palette.add([
-        {
-          id: newTerminalId,
-          args: void 0,
-          category: 'Terminal',
-          text: 'New Terminal',
-          caption: 'Start a new terminal session'
-        }
-      ]);
-      console.log('added to palette here');
-=======
 const terminalExtension = {
   id: 'jupyter.extensions.terminal',
   activate: activateTerminal
@@ -100,7 +53,6 @@ function activateTerminal(app: Application): Promise<void> {
       category: 'Terminal',
       text: 'New Terminal',
       caption: 'Start a new terminal session'
->>>>>>> master
     }
   ]);
 
